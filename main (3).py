5db398b6258a2159e1b2165940e389eacb75304c
Linu0406 <144367827+Linu0406@users.noreply.github.com>
2023-09-27 12:10:42 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,6 @@
+year = int(input("enter a year"))
+if (year % 4 == 0 and year % 100 != year = int(input("enter a year"))
+if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+  print('it is leap year')
+else:
+  print('it is not a leap year ')
